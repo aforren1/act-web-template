ffbeda8b511cac640495eb5b7e9f600659fdc7a6
Alex Forrence <alex.forrence@gmail.com>
2020-07-28 20:07:59 -0400

    server fix?

--- server.py
+++ server.py
@@ -2,18 +2,17 @@ from aiohttp import web
 
 import socketio
 
+path = 'dist'
+sio = socketio.AsyncServer(async_mode='aiohttp')
+app = web.Application()
+sio.attach(app)
+
+async def index(request):
+    with open(f'./{path}/index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+app.router.add_get('/', index)
+app.router.add_static('/', f'./{path}')
+
 if __name__ == '__main__':
-    import sys
-
-    path = 'dist'
-    sio = socketio.AsyncServer(async_mode='aiohttp')
-    app = web.Application()
-    sio.attach(app)
-
-    async def index(request):
-        with open(f'./{path}/index.html') as f:
-            return web.Response(text=f.read(), content_type='text/html')
-
-    app.router.add_get('/', index)
-    app.router.add_static('/', f'./{path}')
     web.run_app(app)
